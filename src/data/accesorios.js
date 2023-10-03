1df410161ff968c8fe82973db60a30232c37b040
Enrique Ginfeu <ginfeu.enrique@hotmail.com>
2023-10-03 19:11:13 -0300

    first commit page ford

---
+++ src/data/accesorios.js
@@ -0,0 +1,428 @@
+export const accesorios = [
+    {
+        id: 'mustang-tuercas',
+        titulo: 'Mustang',
+        descripcion: 'Kit Tuercas Antirrobo',
+        precio: '50.000',
+
+        img: '../src/assets/tuercas1.webp'
+    },
+    {
+        id: 'mustang-calco1',
+        titulo: 'Mustang',
+        descripcion: 'Calcomanías vinilo negro doble linea',
+        precio: '300.000',
+
+        img: '../src/assets/mustang-calco2.webp' 
+    },
+    {
+        id: 'mustang-calco2',
+        titulo: 'Mustang',
+        descripcion: 'Calcomanías vinilo blanca doble linea',
+        precio: '300.000',
+
+        img: '../src/assets/mustang-calco1.webp' 
+    },
+    {
+        id: 'mustang-aleron1',
+        titulo: 'Mustang',
+        descripcion: 'Alerón largo',
+        precio: '560.000',
+
+        img: '../src/assets/mustang-aleron2.webp' 
+    },
+    {
+        id: 'mustang-aleron2',
+        titulo: 'Mustang',
+        descripcion: 'Alerón corto negro satinado',
+        precio: '340.000',
+
+        img: '../src/assets/mustang-aleron1.webp' 
+    },
+    {
+        id: 'kuga-tuercas',
+        titulo: 'Kuga',
+        descripcion: 'Kit Tuercas Antirrobo',
+        precio: '50.000',
+
+        img: '../src/assets/tuercas1.webp'
+    },
+    {
+        id: 'kuga-alfombra',
+        titulo: 'Kuga',
+        descripcion: 'Cubrealfombra de Baúl',
+        precio: '41.000',
+
+        img: '../src/assets/kuga-cubrealfombre.webp' 
+    },
+    {
+        id: 'kuga-cobertor',
+        titulo: 'Kuga',
+        descripcion: 'Kit cobertor espacio de carga',
+        precio: '107.000',
+
+        img: '../src/assets/kuga-cobertor.webp' 
+    },
+    {
+        id: 'kuga-carpa',
+        titulo: 'Kuga',
+        descripcion: 'Carpa Trasera para SUV',
+        precio: '464.000',
+
+        litro: '',
+        img: '../src/assets/kuga-carpa.webp' 
+    },
+    {
+        id: 'bronco-tuercas',
+        titulo: 'Bronco',
+        descripcion: 'Kit Tuercas Antirrobo',
+        precio: '70.000',
+
+        img: '../src/assets/tuercas1.webp'
+    },
+    {
+        id: 'bronco-remolque',
+        titulo: 'Bronco',
+        descripcion: 'Kit Gancho de Remolque',
+        precio: '341.000',
+
+        img: '../src/assets/bronco-remolque.webp'
+    },
+    {
+        id: 'bronco-bicicleta1',
+        titulo: 'Bronco',
+        descripcion: 'Portabicicleta Thule',
+        precio: '213.000',
+
+        img: '../src/assets/bronco-bici2.webp'
+    },
+    {
+        id: 'bronco-barras',
+        titulo: 'Bronco',
+        descripcion: 'Barras Transversales de techo para BIG BEND',
+        precio: '228.000',
+
+        img: '../src/assets/bronco-barras.webp'
+    },
+    {
+        id: 'bronco-bicicleta2',
+        titulo: 'Bronco',
+        descripcion: 'Portabicicleta Yakima',
+        precio: '318.000',
+
+        img: '../src/assets/bronco-bici.webp'
+    },
+    {
+        id: 'bronco-baul',
+        titulo: 'Bronco',
+        descripcion: 'Baúl Portaequipaje 330L',
+        precio: '336.000',
+
+        img: '../src/assets/bronco-baul.webp'
+    },
+    {
+        id: 'bronco-canasta',
+        titulo: 'Bronco',
+        descripcion: 'Portaequipaje tipo canasta con Red',
+        precio: '387.000',
+
+        img: '../src/assets/bronco-canasta.webp'
+    },
+    {
+        id: 'bronco-carpa1',
+        titulo: 'Bronco',
+        descripcion: 'Carpa Trasera para SUV',
+        precio: '464.000',
+
+        img: '../src/assets/bronco-carpa1.webp'
+    },
+    {
+        id: 'bronco-carpa2',
+        titulo: 'Bronco',
+        descripcion: 'Carpa de Techo',
+        precio: '1.810.000',
+
+        img: '../src/assets/bronco-carpa2.webp'
+    },
+    {
+        id: 'maverick-cobertor',
+        titulo: 'Maverick',
+        descripcion: 'Cobertor Retractil de apertura Electrica',
+        precio: '1.093.000',
+
+        img: '../src/assets/maverick-cobertor.webp'
+    },
+    {
+        id: 'maverick-san-antonio',
+        titulo: 'Maverick',
+        descripcion: 'Barra San Antonio',
+        precio: '262.000',
+
+        img: '../src/assets/maverick-san-antonio.webp'
+    },
+    {
+        id: 'maverick-porta-objetos',
+        titulo: 'Maverick',
+        descripcion: 'Porta Objetos Pivotante lado derecho',
+        precio: '191.000',
+
+        img: '../src/assets/maverick-pivotante.webp'
+    },
+    {
+        id: 'maverick-bici',
+        titulo: 'Maverick',
+        descripcion: 'Portabicicleta Yakima',
+        precio: '318.000',
+
+        img: '../src/assets/maverick-bici.webp'
+    },
+    {
+        id: 'maverick-carpa',
+        titulo: 'Maverick',
+        descripcion: 'Carpa de Techo',
+        precio: '1.810.000',
+
+        img: '../src/assets/maverick-carpa.webp'
+    },
+    {
+        id: 'f150-cobertor',
+        titulo: 'F-150',
+        descripcion: 'Cobertor retractil',
+        precio: '1.835.000',
+
+        img: '../src/assets/f150-cobertor.webp'
+    },
+    {
+        id: 'f150-deflector1',
+        titulo: 'F-150',
+        descripcion: 'Deflector de Aire de capo',
+        precio: '95.000',
+
+        img: '../src/assets/f150-deflector1.webp'
+    },
+    {
+        id: 'f150-deflector2',
+        titulo: 'F-150',
+        descripcion: 'Kit Deflectores de aire de ventanillas',
+        precio: '57.000',
+
+        img: '../src/assets/f150-deflector2.webp'
+    },
+    {
+        id: 'f150-organizador',
+        titulo: 'F-150',
+        descripcion: 'Organizador porta objetos para caja de carga',
+        precio: '140.000',
+
+        img: '../src/assets/f150-organizador.webp'
+    },
+    {
+        id: 'f150-bici',
+        titulo: 'F-150',
+        descripcion: 'Funda portabicicletas para tapa de caja de carga',
+        precio: '252.000',
+ 
+        img: '../src/assets/f150-bici.webp'
+    },
+    {
+        id: 'f150-carpa',
+        titulo: 'F-150',
+        descripcion: 'Carpa para caja de carga',
+        precio: '442.000',
+
+        img: '../src/assets/f150-carpa.webp'
+    },
+    {
+        id: 'f150-raptor-cobertor',
+        titulo: 'F-150 Raptor',
+        descripcion: 'Cobertor retractil',
+        precio: '1.835.000',
+ 
+        img: '../src/assets/f150-cobertor.webp'
+    },
+    {
+        id: 'f150-raptor-deflector',
+        titulo: 'F-150 Raptor',
+        descripcion: 'Kit Deflectores de Aire de Ventanillas',
+        precio: '57.000',
+
+        img: '../src/assets/f150-raptor-deflector.webp'
+    },
+    {
+        id: 'f150-raptor-red-horizontal',
+        titulo: 'F-150 Raptor',
+        descripcion: 'Red Sujecion de carga horizontal',
+        precio: '64.000',
+
+        img: '../src/assets/f150-raptor-horizontal.webp'
+    },
+    {
+        id: 'f150-raptor-red-vertical',
+        titulo: 'F-150 Raptor',
+        descripcion: 'Red Sujecion de carga vertical',
+        precio: '64.000',
+
+        img: '../src/assets/f150-raptor-vertical.webp'
+    },
+    {
+        id: 'f150-raptor-carpa',
+        titulo: 'F-150 Raptor',
+        descripcion: 'Carpa para caja de carga',
+        precio: '442.000',
+
+        img: '../src/assets/f150-carpa.webp'
+    },
+    {
+        id: 'ranger-cobertor',
+        titulo: 'Ranger',
+        descripcion: 'Cobertor Retractil Electrico',
+        precio: '1.190.000',
+
+        img: '../src/assets/ranger-cobertor.webp'
+    },
+    {
+        id: 'ranger-protector-radiador',
+        titulo: 'Ranger',
+        descripcion: 'Protector de radiador',
+        precio: '52.000',
+
+        img: '../src/assets/ranger-protector-radiador.webp'
+    },
+    {
+        id: 'ranger-antirrobo',
+        titulo: 'Ranger',
+        descripcion: 'Traba antirrobo malacate rueda de auxilio',
+        precio: '36.000',
+
+        img: '../src/assets/antirrobo.webp'
+    },
+    {
+        id: 'ranger-deflector',
+        titulo: 'Ranger',
+        descripcion: 'Deflectores de Ventanillas',
+        precio: '43.000',
+
+        img: '../src/assets/f150-deflector2.webp'
+    },
+    {
+        id: 'ranger-deflector-capo',
+        titulo: 'Ranger',
+        descripcion: 'Deflector de Capó',
+        precio: '77.000',
+
+        img: '../src/assets/ranger-protector-capo.webp'
+    },
+    {
+        id: 'ranger-protector-luces',
+        titulo: 'Ranger',
+        descripcion: 'Kit Protector de Opticas Delanteras Led',
+        precio: '76.000',
+
+        img: '../src/assets/ranger-protector-luces.webp'
+    },
+    {
+        id: 'ranger-barras',
+        titulo: 'Ranger',
+        descripcion: 'Barras Transversales para Caja de Carga',
+        precio: '431.000',
+
+        img: '../src/assets/ranger-barras.webp'
+    },
+    {
+        id: 'ranger-bici',
+        titulo: 'Ranger',
+        descripcion: 'Portabicicleta Yakima',
+        precio: '318.000',
+
+        img: '../src/assets/ranger-bici.webp'
+    },    
+    {
+        id: 'ranger-carpa',
+        titulo: 'Ranger',
+        descripcion: 'Carpa para Caja de Carga',
+        precio: '322.000',
+
+        img: '../src/assets/ranger-carpa.webp'
+    },
+    {
+        id: 'ranger-snorkel',
+        titulo: 'Ranger',
+        descripcion: 'Snorkel',
+        precio: '714.000',
+
+        img: '../src/assets/ranger-snorkel.webp'
+    },
+    {
+        id: 'ranger-raptor-cobertor',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Cobertor Retractil Electrico',
+        precio: '1.190.000',
+
+        img: '../src/assets/ranger-cobertor.webp'
+    },
+    {
+        id: 'ranger-raptor-llanta',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Llanta de Aleación de 17" con Capacidad Beadlock (Solo Raptor)',
+        precio: '352.000',
+
+        img: '../src/assets/ranger-raptor-llanta.webp'
+    },
+    {
+        id: 'ranger-raptor-antirrobo',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Traba antirrobo malacate rueda de auxilio',
+        precio: '36.000',
+
+        img: '../src/assets/antirrobo.webp'
+    },
+    {
+        id: 'ranger-raptor-deflector-capo',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Deflector de Capó',
+        precio: '77.000',
+
+        img: '../src/assets/ranger-protector-capo.webp'
+    },
+    {
+        id: 'ranger-raptor-deflector',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Deflectores de Ventanillas',
+        precio: '43.000',
+
+        img: '../src/assets/f150-deflector2.webp'
+    },
+    {
+        id: 'ranger-raptor-protector-luces',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Kit Protector de Opticas Delanteras Led',
+        precio: '76.000',
+
+        img: '../src/assets/ranger-protector-luces.webp'
+    },
+    {
+        id: 'ranger-raptor-barras',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Barras Transversales para Caja de Carga',
+        precio: '431.000',
+
+        img: '../src/assets/ranger-barras.webp'
+    },
+    {
+        id: 'ranger-raptor-bici',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Portabicicleta Yakima',
+        precio: '318.000',
+
+        img: '../src/assets/ranger-bici.webp'
+    },
+    {
+        id: 'ranger-raptor-carpa',
+        titulo: 'Ranger Raptor',
+        descripcion: 'Carpa para Caja de Carga',
+        precio: '322.000',
+
+        img: '../src/assets/ranger-carpa.webp'
+    }
+
+]
---
+++ src/data/autos.js
@@ -0,0 +1,148 @@
+export const autos = [
+    {
+        id: '1',
+        title: 'Territory',
+        description: {
+            motor: 'Motor 1.8L EcoBoost generando 185 CV de potencia y 30 Nm de torque, combinado con una transmisión automática de doble.',
+            interior: 'Ofrece la combinación perfecta de diseño y funcionalidad. Diseñada para maximizar el confort con un espacio interior para disfrutar de cualquier viaje.',
+            tecnologia: 'Cuenta con un nuevo sistema multimedia de 12" compatible con Apple CarPlay & Android Auto inalámbrico y con un tablero digital configurable de 7".',
+            colores: 'Gris Mercurio, Azul Metálico, Blanco Oxford.',
+            transmision: 'Automatica de 7 Velocidades.'
+        },
+        img: '../src/assets/territory.webp',
+        img1: '../src/assets/territory1.webp',
+        img2: '../src/assets/territory2.webp',
+        img3: '../src/assets/territory3.webp',
+        img4: '../src/assets/territory4.webp'
+
+    },
+    {
+        id: '2',
+        title: 'Ranger Limited+',
+        description: {
+            motor: ' motor 3.0L V6 Turbo Diesel que ofrece 250CV de potencia y 600 Nm de torque combinados con una caja automática secuencial',
+            interior: 'La nueva Ranger Limited+ cuenta con Asiento conductor c/ajuste eléctrico en 8 direcciones. Asientos con tapizado de cuero ecológico. Sistema de audio con 6 parlantes. Cargador inalámbrico. 2 filas de entradas USB (2 tipo A + 2 tipo C).',
+            tecnologia: 'a Nueva Ranger se destaca por su tencología innovadora, incorporando una pantalla multitactil de 12“ con alta definición y un panel de instrumentos LED de 12,4“.',
+            colores: 'Plata Metalizado, Gris Magnetic, Azul Belice, Rojo Bari, Negro Perlado y Naranja Terra.',
+            transmision: 'Automatica de 10 Velocidades.'
+        },
+        img: '../src/assets/ranger.webp',
+        img1: '../src/assets/ranger1.webp',
+        img2: '../src/assets/ranger2.webp',
+        img3: '../src/assets/ranger3.webp',
+        img4: '../src/assets/ranger4.webp'
+         
+    },
+    {
+        id: '3',
+        title: 'Mustang',
+        description: {
+            motor: 'Motor V8 5.0L Inyeccion Dual con doble arbol de levas a la cabeza, 4 Valvulas por cilindro con apertura variable y distribucion independiente.',
+            interior: 'La calidad percibida ha mejorado considerablemente y los gráficos de las pantallas son más claros gracias a la tecnología Unreal Engine 3D que encontramos en el mundo de los videojuegos. El climatizador se ha integrado en la pantalla táctil, hay un volante con el fondo plano, un punto de carga inalámbrico, puertos USB y asientos de tela o cuero.',
+            tecnologia: 'Esta nueva generacion de Mustang cuenta con sistema de conectividad SYNC 3 con pantalla multitactil de 8" y nevegador satelital, control po voz y compatibilidad con Android Auto y Apple CarPlay. Sistema de audio Bang & Olufsen con 12 parlantes y camara de estacionamiento trasera.',
+            colores: 'Blanco Oxford, Gris Mercurio y Negro Ebony',
+            transmision: 'Automatica de 10 Velocidades.'
+        },
+        img: '../src/assets/mustang7gen.webp',
+        img1: '../src/assets/mustang1.webp',
+        img2: '../src/assets/mustang2.webp',
+        img3: '../src/assets/mustang3.webp',
+        img4: '../src/assets/mustang4.webp'
+    },
+    {
+        id: '4',
+        title: 'Ranger Raptor',
+        description: {
+            motor: 'La nueva Ranger Raptor cuenta con un motor naftero EcoBoost 3.0L V6 Bi-Turbo brindando 397 CV de potencia y 583 NM de troque, Tambien cuenta con una transmision automatica.',
+            interior: 'El interior de la Ranger Raptor cuenta con 7 Airbags, frontales, de cortina, laterales y de rodilla solo para el conductor. Ofrece 5 apoyacabezas regulables en altura, anclajes de seguridad ISOFIX en las plazas traseras y un sistema de frenado post colision, con activacion automatica de balizas, bocina y desbloqueo de puertas. Tambien cuenta con el sistema de gestion de terrenos de 7 modos de manejo (Normal, Deportivo, Resbaladizo, Barro/Surcos, Arena, Rocas y Baja).',
+            tecnologia: 'La Nueva Ranger Raptor cuenta con cámara 360°. Ofrece camaras en la parte delantera, trasera y en los laterales de la Pick-Up para ayudar al conductor en espacios reducidos. Tambien cuenta con Ford Co-pilot 360, tecnologia de asistencia en la conduccion para mejorar la seguridad en cada viaje.',
+            colores: 'Naranja Metálico, Blanco Ártico, Gris Piedra y Azul Performance.',
+            transmision: 'Automatica de 10 Velocidades.'
+        },
+        img: '../src/assets/raptor.webp',
+        img1: '../src/assets/ranger-raptor1.webp',
+        img2: '../src/assets/ranger-raptor2.webp',
+        img3: '../src/assets/ranger-raptor3.webp',
+        img4: '../src/assets/ranger-raptor4.webp'
+    },
+    {
+        id: '5',
+        title: 'Maverick',
+        description: {
+            motor: 'La nueva Maverick te ofrece el mejor desempeño a traves 2 motorizaciones. La Maverick Hibrida con un motor naftero de 2.5L ciclo Atkinson y otro electrico autorecargable. Por otro lado, la version naftera cuenta con un motor EcoBoost de 2.0L.',
+            interior: 'El interior cuenta con una pantalla tactil de 8" con SYNC 2.5. El sistema SYNC 2.5 es compatible con Apple CarPlay y Android Auto. Tambien cuenta con una panralla digital de 6.5" para el tablero de instrumentos',
+            tecnologia: 'La nueva Maverick utiliza tecnologia de avanzada aplicada a la seguridad y garantiza una conduccion confiable y placentera. Tambien cuenta con 5 modos de conduccion.',
+            colores: 'Gris Mercurio, Gris Plata, Gris Cactus, Azul Relampago, Negro Pantera y Blanco Oxford',
+            transmision: 'Automatica de 8 Velocidades',
+        },
+        img: '../src/assets/maverick.webp',
+        img1: '../src/assets/maverick1.webp',
+        img2: '../src/assets/maverick2.webp',
+        img3: '../src/assets/maverick3.webp',
+        img4: '../src/assets/maverick4.webp'
+    },
+    {
+        id: '6',
+        title: 'Bronco',
+        description: {
+            motor: 'La Bronco cuenta con la mejor conbinacion de potencia y torque para enfrentar cualquier desafio gracias a su motorizacion EcoBoost de 1.5L (175CV) y 2.0L (253CV).',
+            interior: 'El asiento con bolsillo portaobjetos posee cierre para proteger las pertenencias más delicadas durante el viaje. Climatización automática con salida de aire en las plazas traseras. El rotary shifter (selector giratorio de marchas) incluido en la Bronco Sport permite elegir el cambio de forma simple y rápida, facilitando la conducción durante la aventura. Ademas cuenta con 9 airbags.',
+            tecnologia: 'En el interior cuenta con una pantalla tactil de 8" con SYNC 3, compatible con Apple CarPlay y Android Auto. La Bronco tiene un sistema de gestion de terrenos con 7 modos de manejo.',
+            colores: 'Gris Mercurio, Gris Plata, Gris Cactus, Negro Pantera y Blanco Oxford',
+            transmision: 'Automatica de 8 Velocidades',
+        },
+        img: '../src/assets/bronco.webp',
+        img1: '../src/assets/bronco1.webp',
+        img2: '../src/assets/bronco2.webp',
+        img3: '../src/assets/bronco3.webp',
+        img4: '../src/assets/bronco4.webp'
+    },
+    {
+        id: '7',
+        title: 'Kuga Hibrida',
+        description: {
+            motor: 'La nueva Kuga Hibrida esta equipada con una motorizacion de 2 motores, un 2.5L Naftero y uno eléctrico autorecargable de 203 CV de potencia',
+            interior: 'En la cabina, encontrarás detalles exclusivos en las puertas y en el panel de instrumentos, un volante térmico, una pantalla táctil SYNC 4 de 13.2” y más detalles de lujo. Incluye un increíble techo solar panorámico deslizable, con barras de techo longitudinales. Asientos tapizados en cuero con diseño acolchado y microperforaciones. Plazas delanteras calefaccionadas. Volante multifunción calefaccionado forrado en cuero. Asientos delanteros con ajustes eléctricos.',
+            tecnologia: 'Cuenta con una Pantalla multitáctil de 13,2” con SYNC 4 compatible con Android Auto y Apple CarPlay al igual que el Tablero Digital Configurable de 12,3", Desde donde el conductor podrá visualizar y controlar las distintas funcionalidades de la camioneta, sin quitar las manos del volante. Sistema Keyless, que es el sistema de ingreso y arranque sin llaves, comenzás el viaje sin sacar la llave del bolsillo. Sistema de gestión de terrenos con 4 modos de manejo (Normal, ECO, Resbaladizo y Sport).',
+            colores: 'Gris Mercurio, Azul Vapor, Plata Lunar, Negro Pantera y Blanco Oxfrod',
+            transmision: 'Automatica de 8 Velocidades',
+        },
+        img: '../src/assets/kuga.webp',
+        img1: '../src/assets/kuga1.webp',
+        img2: '../src/assets/kuga2.webp',
+        img3: '../src/assets/kuga3.webp',
+        img4: '../src/assets/kuga4.webp'
+    },
+    {
+        id: '8',
+        title: 'F-150 Hibrida',
+        description: {
+            motor: 'La F-150 Hibrida cuenta con motorizacion de 2 motores, Motor PowerBoost 3.5L V6 Hibrido compuesto por 2 motores combinados que ofrecen una pótencia maxima de 436CV y 772 Nm de torque.',
+            interior: 'Su interior esta equipado con 8 Airbags. Sistema de almacenamiento electrico, el Pro Power On Board es un sistema que permite que la bateria de alto voltaje se recargue durante la marcha.',
+            tecnologia: 'Tablero de instrumentos con una pantalla 100% digital de 12". Cuenta con camara 360° con pantalla dividida y sesores traseros. Pantalla multitáctil de 12” con SYNC 4 compatible con Apple CarPlay & Android Auto, con conectividad inalámbrica del celular. Keyless, sistema de ingreso y arranque sin llaves, comenzás el viaje sin sacar la llave del bolsillo. Sistema de Información de Punto Ciego (BLIS) Con Alerta De Tráfico Cruzado. Asistente de dirección evasiva. Control Automático de Luces Altas. Asistente de pre-colisión con frenado automático de emergencia y detección de peatones.',
+            colores: 'Negro Ebony, Roja Racing y Blanco Metalizado.',
+            transmision: 'Automatica de 10 Velocidades',
+        },
+        img: '../src/assets/f-150.webp',
+        img1: '../src/assets/f1501.webp',
+        img2: '../src/assets/f1502.webp',
+        img3: '../src/assets/f1503.webp',
+        img4: '../src/assets/f1504.webp'
+    },
+    {
+        id: '9',
+        title: 'F-150 Raptor',
+        description: {
+            motor: 'La F-150 Raptor cuenta con un motor EcoBoost 3.5L V6 HO, es un motor de alto rendimiento con doble Turbo Compresor para maximizar la potencia y el torquedel motor que da como resultado unos 465 CV y 691 Nm de torque.',
+            interior: 'Su interior esta equipado con 8 Airbags. El sistema de gestión del terreno permite al conductor seleccionar entre los modos pre-establecidos para mejorar la dinámica de conducción en una variedad de terrenos. Estos modos van a modificar los distintos parámetros del vehículo para maximizar la performance en cada tipo de suelo.',
+            tecnologia: 'Tablero de instrumentos con una pantalla 100% digital de 12". Cuenta con camara 360° con pantalla dividida y sesores traseros. Pantalla multitáctil de 12” con SYNC 4 compatible con Apple CarPlay & Android Auto, con conectividad inalámbrica del celular. Keyless, sistema de ingreso y arranque sin llaves, comenzás el viaje sin sacar la llave del bolsillo. Sistema de Información de Punto Ciego (BLIS) Con Alerta De Tráfico Cruzado. Asistente de dirección evasiva. Control Automático de Luces Altas. Asistente de pre-colisión con frenado automático de emergencia y detección de peatones.',
+            colores: 'Azul Relampago, Blanco Oxford y Negro Ebony.',
+            transmision: 'Automatica de 10 Velocidades',
+        },
+        img: '../src/assets/f-150-raptor.webp',
+        img1: '../src/assets/f150-raptor1.webp',
+        img2: '../src/assets/f150-raptor2.webp',
+        img3: '../src/assets/f150-raptor3.webp',
+        img4: '../src/assets/f150-raptor4.webp'
+    }
+]
